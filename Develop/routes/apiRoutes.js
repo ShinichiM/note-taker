040fe879719ce4a589dc83cb13fcbd1189d13574
Shinichi <miyakawashi@gmail.com>
2022-01-23 12:47:23 -0500

    delete route for notes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 const uuid = require('uuid');
-var holdData;
+var holdData = [];
 
 router.get('/notes', (req,res) => {
     fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
@@ -27,6 +27,27 @@ router.post('/notes', ({ body },res) => {
         }
     );
         res.json(body);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    holdData.forEach((item, index) => {
+        if (item.id === req.params.id) {
+            holdData.splice(index, 1);
+        }
+    })
+    fs.writeFile(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(holdData),
+        (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+        }); 
+    res.json({
+        message: 'successful removal',
+        data: req.params.id
+    });
 });
 
 module.exports = router;
